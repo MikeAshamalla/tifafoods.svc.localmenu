a8981b8b25002c62388b42b293ab7df1ed662e2e
MikeAshamalla <mike@ashamalla.com>
2021-05-31 12:50:20 -0700

    Initial dev commit

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,3 +1,5 @@
+#This micro service fetches the local menu from the tfiweb database and returns the results as json.
+
 def lambda_handler(event, context):
     #get location from the GET call
     
