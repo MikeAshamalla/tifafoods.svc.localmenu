80e438e3f5aabaa73018e1214cd2983875156eb4
Mike Ashamalla <59103116+MikeAshamalla@users.noreply.github.com>
2021-07-03 16:46:34 -0700

    Update tfilocalmenu.js

--- tfilocalmenu.js
+++ tfilocalmenu.js
@@ -1,165 +1,19 @@
 <script>
-	jQuery(window).load(function($) {
-
-		var str = `
-{"response": 
-    {
-    "pricedItems": [
-        {
-            "category": "Drinks: Coffee",
-            "subCategory": "",
-            "items": [
-                {
-                    "name": "Espresso",
-                    "priceList": [
-                        {
-                            "size": "Single",
-                            "price": "2.75"
-                        },
-                        {
-                            "size": "Double",
-                            "price": "2.95"
-                        }
-                    ]
-                },
-                {
-                    "name": "Latte",
-                    "priceList": [
-                        {
-                            "size": "12 oz",
-                            "price": "4.45"
-                        },
-                        {
-                            "size": "16 oz",
-                            "price": "5.20"
-                        },
-                        {
-                            "size": "20 oz",
-                            "price": "5.95"
-                        }
-                    ]
-                },
-                {
-                    "name": "Macchiato",
-                    "priceList": [
-                        {
-                            "size": "Single",
-                            "price": "3.15"
-                        },
-                        {
-                            "size": "Double",
-                            "price": "3.70"
-                        }
-                    ]
-                },
-                {
-                    "name": "Mocha",
-                    "priceList": [
-                        {
-                            "size": "12 oz",
-                            "price": "5.20"
-                        },
-                        {
-                            "size": "16 oz",
-                            "price": "6.20"
-                        },
-                        {
-                            "size": "20 oz",
-                            "price": "7.20"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "category": "Gelato",
-            "subCategory": "",
-            "items": [
-                {
-                    "name": "Gelato Cup",
-                    "priceList": [
-                        {
-                            "size": "Small",
-                            "price": "5.20"
-                        },
-                        {
-                            "size": "Medium",
-                            "price": "6.20"
-                        },
-                        {
-                            "size": "Large",
-                            "price": "7.20"
-                        }
-                    ]
-                },
-                {
-                    "name": "Gelato-Pint",
-                    "priceList": [
-                        {
-                            "size": "",
-                            "price": "12.00"
-                        }
-                    ]
-                },
-                {
-                    "name": "Gelato-Quart",
-                    "priceList": [
-                        {
-                            "size": "",
-                            "price": "20.00"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "category": "Baked Goods: Cookie & Brownie",
-            "subCategory": "",
-            "items": [
-                {
-                    "name": "Tifa Cookie",
-                    "priceList": [
-                        {
-                            "size": "",
-                            "price": "2.00"
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-    ,
-    "groupPricedItems": 
-        [
-            {"category": "Gelato",
-            "subCategory": "Current Flavors",
-            "itemList": ["Dark Chocolate", "Cookie Butter", "Vanilla"],
-            "priceList": [{"size": "Small", "price": "4.95"}, {"size": "Medium", "price": "5.95"}, {"size": "Large", "price": "6.95"}, {"size": "Pint", "price": "12.95"}, {"size": "Quart", "price": "19.95"}]
-            }
-        ]
-    }
-}
-`;
-
-
-		
-		var parsed = JSON.parse(str);
-		var pricedItems = parsed.response.pricedItems;
-		var groupPricedItems = parsed.response.groupPricedItems;
-		
-
-		function addGroupPricedItems(items) {
-			groupPricedItems.forEach(function (item, key) {
-				if (item.subCategory != '') {
-					jQuery('#tfilocalmenu').append('<h2>' + item.subCategory + '</h2>');
-				}
-				item.itemList.forEach(function (item, key) {
-					jQuery('#tfilocalmenu').append('<ul>' + item + '</ul>');
-				});
+	function addGroupPricedItems(items) {
+		items.forEach(function (item, key) {
+			if (item.subCategory != '') {
+				jQuery('#tfilocalmenu').append('<h2>' + item.subCategory + '</h2>');
+			}
+			item.itemList.forEach(function (item, key) {
+				jQuery('#tfilocalmenu').append('<ul>' + item + '</ul>');
 			});
-		}
+		});
+	}
 
-
+	function displayItems(data) {
+		var pricedItems = data.response.pricedItems;
+		var groupPricedItems = data.response.groupPricedItems;	
+		
 		pricedItems.forEach(function (item, key) {
 			jQuery('#tfilocalmenu').append('<h1>' + item.category + '</h1>');
 			if (item.category == 'Gelato') {
@@ -181,7 +35,15 @@
 				jQuery('#tfilocalmenu').append('<tr>' + ulLine + '</tr>');
 			})
 			jQuery('#tfilocalmenu').append('</tbody></table></figure>');
-		});
+		});		
+	}
+
+	jQuery(window).load(function($) {
+
+		var jqxhr = jQuery.getJSON('https://1jf6x7kd5a.execute-api.us-west-2.amazonaws.com/Prod/tifafoods_svc_localmenu?location=F00000-1')
+			.done(function( data ) {
+	    displayItems(data);
+	  });
 
 	});
 </script>
